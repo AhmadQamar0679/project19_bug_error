2a91009b50302c54ab0d7776fdf8944d5dbfe517
Ahmad Qamar <ahmadqamar.rq@gmail.com>
2026-05-07 13:50:13 +0500

    .

--- a_main/b_bug/views.py
+++ a_main/b_bug/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
+    messages.debug(request,'This is dbug message.')
+    messages.info(request,'This is an info message')
     return render (request,'home.html')
